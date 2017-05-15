2379a1c6ceda1afe1e2f8d8065eb3a0483373d5b
Phang Chun Rong <phangcr@garena.com>
2017-05-15 11:57:40 +0800

    First Commit

---
+++ webpack.config.js
@@ -0,0 +1,60 @@
+var path = require('path');
+var webpack = require('webpack');
+var merge = require('webpack-merge');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const TARGET = process.env.npm_lifecycle_event;
+process.env.BABEL_ENV = TARGET;
+
+const PATHS = {
+	app: path.join(__dirname, 'app'),
+	build: path.join(__dirname, 'build')
+}
+
+const common = {
+	entry: PATHS.app,
+	output: {
+		path: PATHS.build,
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.js', '.jsx']
+	},
+	module: {
+		loaders: [
+		    {
+		      test: /\.js?$/,
+		      loaders: ['babel-loader'],
+		      include: PATHS.app
+		    }
+		]
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			title: 'BlackCat',
+			template: 'my-index.ejs', // Load a custom template (ejs by default see the FAQ for details)
+		})
+	]
+}
+
+if (TARGET === 'start' || !TARGET) {
+  module.exports = merge(common, {
+    devServer: {
+      historyApiFallback: true,
+      hot: true,
+      inline: true,
+      stats: 'errors-only',
+
+      host: process.env.HOST,
+      port: process.env.PORT
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin()
+    ],
+    devtool: 'eval-source-map'
+  });
+}
+
+if (TARGET === 'build') {
+  module.exports = merge(common, {})
+}
